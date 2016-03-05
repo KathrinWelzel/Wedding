524a7c77efc15a4d5a7b94dd12577f6a82e1b2ba
Nishant Gaurav <iamnishantg@gmail.com>
2016-03-06 00:38:10 +0530

    bootcamp.generic_follow

--- bootcamp/generic_follow/urls.py
+++ bootcamp/generic_follow/urls.py
@@ -3,17 +3,17 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(
         r'^follow/(?P<app>[^\/]+)/(?P<model>[^\/]+)/(?P<object_id>\d+)/$',
-        'followit.views.follow_object',
+        'bootcamp.generic_follow.views.follow_object',
         name = 'follow_object'
     ),
     url(
         r'^unfollow/(?P<app>[^\/]+)/(?P<model>[^\/]+)/(?P<object_id>\d+)/$',
-        'followit.views.unfollow_object',
+        'bootcamp.generic_follow.views.unfollow_object',
         name = 'unfollow_object'
     ),
     url(
         r'^toggle-follow/(?P<app>[^\/]+)/(?P<model>[^\/]+)/(?P<object_id>\d+)/$',
-        'followit.views.toggle_follow_object',
+        'bootcamp.generic_follow.views.toggle_follow_object',
         name='toggle_follow_object'
     )
 )
