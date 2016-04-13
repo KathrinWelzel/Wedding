2d8f5606b9346e7eb6aec796ab5caaf4e59e8149
marcel.grossmann <marcel.grossmann@uni-bamberg.de>
2016-04-13 23:14:01 +0200

    Add View Update

--- src/bootcamp/photos/views.py
+++ src/bootcamp/photos/views.py
@@ -1,8 +1,22 @@
 from django.contrib.auth.decorators import login_required
-from django.views.generic import CreateView
+from django.forms import ModelForm
+from django.views.generic.edit import FormView
+from django.http import HttpResponseRedirect
 from photologue.models import Photo
+from photologue.views import PhotoDetailView
 from .models import PhotoExtended
 
-class AddPhotoView(CreateView):
-    model = Photo
-    fields = ['title', 'slug', 'caption', 'image']
+class PhotoForm(ModelForm):
+    class Meta:
+        model = Photo
+        fields = ['title', 'slug', 'caption', 'image']
+
+
+class AddPhotoView(FormView):
+    template_name = 'photologue/photo_form.html'
+    form_class = PhotoForm
+    success_url = '/photos/photo/'
+
+    def form_valid(self, form):
+        new_photo = form.save()
+        return HttpResponseRedirect(self.get_success_url() + new_photo.slug)
